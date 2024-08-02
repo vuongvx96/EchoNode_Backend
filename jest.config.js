87cff07cfc459142d9f213f6095541c6dfa82ef5
Vo, Vuong <vuong.vo@zoi.tech>
2024-08-02 21:19:54 +0700

    adding module mappers jest

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,10 @@ module.exports = {
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
+  moduleNameMapper: {
+    "@core/(.*)$": "<rootDir>/src/core/$1",
+    "@infrastructure/(.*)$": "<rootDir>/src/infrastructure/$1",
+    "@application/(.*)$": "<rootDir>/src/application/$1",
+    "@test/(.*)$": "<rootDir>/test/$1"
+  },
 };
